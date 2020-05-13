27d0ee7cad6d167757d112807c3ba7a95680c1fd
durok <durok@firemail.cc>
2020-05-13 16:28:56 +0200

    Tx: Modulation: Working 4/16/64 QAM, normalization error Normalization term for 64 QAM does not scale outer symbols to 1, instead to 1.08012

--- transmitter/map2symbols.m
+++ transmitter/map2symbols.m
@@ -3,20 +3,30 @@ function d = map2symbols(c, constellation_order, switch_graph)
      d = []; 
   else
     % set parameters
-    n_bits = 2^constellation_order;
-    G1 = [1 1 0 0;
-          0 1 1 0;
-          0 0 1 1;
-          0 0 0 1];
-    G2 = [1 1;
-          0 1];
+    n_bits = constellation_order;
     n_words = length(c)/n_bits;
     w = reshape(c, [n_bits n_words])';
+    % Normalization factor
+    qnorm = sqrt(2*(2^constellation_order-1)/3)
     % QAM -- cut data word in half, gray mapping, BIN to DEC conversion, cartesian mapping, offset/rescale
-    c = [mod(w(:,1:end/2)*G2, 2) mod(w(:,end/2+1:end)*G2, 2)];
-    y = ( (bi2de(c(:,1:end/2), 'left-msb')*2-3) + 1j*(bi2de(c(:,end/2+1:end), 'left-msb')*2-3) ) / sqrt(3^2+1);
+    switch constellation_order
+      case 2
+        d = ( (bi2de(w(:,1:end/2), 'left-msb')*2-1) + 1j*(bi2de(w(:,end/2+1:end), 'left-msb')*2-1) ) / qnorm;
+      case 4
+        G = [1 1;
+          0 1];
+        c = [mod(w(:,1:end/2)*G, 2) mod(w(:,end/2+1:end)*G, 2)];
+        d = ( (bi2de(c(:,1:end/2), 'left-msb')*2-3) + 1j*(bi2de(c(:,end/2+1:end), 'left-msb')*2-3) ) / qnorm;
+      case 6
+        G = [1 1 1;
+            0 1 1;
+            0 0 1];
+        c = [mod(w(:,1:end/2)*G,2) mod(w(:,end/2+1:end)*G,2)];
+        d = ( (bi2de(c(:,1:3), 'left-msb')*2-7) + 1j*(bi2de(c(:,4:6), 'left-msb')*2-7) ) / qnorm; 
+    end
+    
     % graphical output
-    if showflag
-        plot(real(y), imag(y), 'o');
+    if switch_graph
+        plot(real(d), imag(d), 'o');
     end
 end
