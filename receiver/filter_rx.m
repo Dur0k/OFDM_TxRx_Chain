09464b015bbf60c2b2da19a159e5898ec6b43627
durok <durok@firemail.cc>
2020-05-25 08:19:50 +0200

    Tx/Rx Filter

--- receiver/filter_rx.m
+++ receiver/filter_rx.m
@@ -1,3 +1,29 @@
-function z_tilde = filter_rx(s_tilde, downsampling_factor, switch_graph, switch_off)
-  
+function z_tilde = filter_rx( s_tilde, downsampling_factor, switch_graph, switch_off )
+
+if switch_off
+    % exception handling
+    z_tilde = s_tilde;
+else
+    % design filter impulse response
+    [iFiltOrd,fo,ao,w] = firpmord([0.4 0.6], [1 0], [10^(-3/20) 10^(-45/20)]);
+    iFiltOrd = ceil(iFiltOrd/2)*2; ... ceil to next even order
+    h = firpm(iFiltOrd, fo, ao, w);
+    g = h/sqrt(sum(h.^2));
+    % AD conversion and impulse shaping
+    d = conv(g,s_tilde)/downsampling_factor*sum(g);
+    % downsampling
+    z_tilde = d(1+iFiltOrd:downsampling_factor:end-iFiltOrd);
+end
+
+% graphical output
+if switch_graph
+    [H,Omega] = freqz(g,1);
+    figure;
+    plot(Omega, 20*log10(abs(H)));
+    xlim([0 pi]);
+    xlabel('\omega/f_s');
+    ylabel('|G_{RX}(j\omega)|^2    (log)');
+    title('spectral magnitude of g_{RX}');
+end
+
 end
--- transmitter/filter_tx.m
+++ transmitter/filter_tx.m
@@ -1,11 +1,29 @@
-function s = filter_tx(z, oversampling_factor, switch_graph, switch_off)
-  zero_vec = zeros(oversampling_factor-1,length(z));
-  tmp = [z.';zero_vec];
-  z_up = tmp(:);
-  %b = fir1(64,0.4,hamming(65));
-  b = fir1(61,3328/(3328/2),'low');
-  s = filter(b,1,z_up);
-  figure;
-  [psd,f] = periodogram(s, rectwin(length(s)), length(s)*2,length(s));
-  plot(fftshift(f),10*log10(fftshift(psd)));
+function s = filter_tx( z, oversampling_factor, switch_graph, switch_off )
+
+if switch_off
+    % exception handling
+    s = z;
+else
+%     design filter impulse response
+    [iFiltOrd,fo,ao,w] = firpmord([0.4 0.6], [1 0], [10^(-3/20) 10^(-45/20)]);
+    iFiltOrd = ceil(iFiltOrd/2)*2; ... ceil to next even order
+    h = firpm(iFiltOrd, fo, ao, w);
+    g = h/sqrt(sum(h.^2));
+    % oversampling
+    d = [z.'; zeros(oversampling_factor-1, length(z))];
+    % DA conversion and impulse shaping
+    s = conv(g,d(:))*oversampling_factor/sum(g);
+end
+
+% graphical output
+if switch_graph
+    [H,Omega] = freqz(g,1);
+    figure;
+    plot(Omega, 20*log10(abs(H)));
+    xlim([0 pi]);
+    xlabel('\omega/f_s');
+    ylabel('|G_{TX}(j\omega)|^2    (log)');
+    title('spectral magnitude of g_{TX}');
+end
+
 end
